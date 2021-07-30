cf4e5d8ed724c7cd9508c22e2211bdcca6ad829e
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2021-07-30 09:31:52 +0200

    Rework repository layout

--- update-gradle-smp-plugin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-go build -o "${SCRIPT_DIR}/update-gradle-smp-plugin" "${SCRIPT_DIR}/update-gradle-smp-plugin.go"
